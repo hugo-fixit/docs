4e650b486a69d501ea6a9afecf9ae42d921568e6
Cell <1024@lruihao.cn>
2022-11-30 21:11:40 +0800

    苟利国家生死以,岂因祸福避趋之

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,10 +6,11 @@ const Docs = new (function () {
   /**
    * Rest in Peace. R.I.P 🕯️
    * @2022-3-28 [3.21-mu5735] 沉痛哀悼 132 名遇难同胞：东航航班失事，遇难者含旅客 123 人，机组 9 人
+   * @2022-12-03 江泽民同志逝世，享年96岁
    * @returns {Docs}
    */
   this.RIP = () => {
-    if (new Date() < new Date('2022-03-31')) {
+    if (new Date() < new Date('2022-12-03')) {
       document.querySelector('html').style.filter = 'grayscale(100%)';
     }
     return this;
@@ -31,6 +32,6 @@ const Docs = new (function () {
   // Docs.init();
   // It will be executed when the DOM tree is built.
   document.addEventListener('DOMContentLoaded', () => {
-    // Docs.init();
+    Docs.init();
   });
 })();
